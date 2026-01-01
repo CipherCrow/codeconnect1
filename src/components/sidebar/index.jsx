496c8fe24af4b36a0864788fc9173b7602cdb792
CipherCrow <jonastorquatro@gmail.com>
2025-12-31 21:36:52 -0300

    estilizando painel lateral

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -3,36 +3,37 @@ import Feed from './assets/feed.svg'
 import Perfil from "./assets/account_circle.svg"
 import Info from "./assets/info.svg"
 import Sair from "./assets/logout.svg"
+import "./styles.css"
 
 export default function Sidebar(){
     return(
         <aside>
             <img src={Logo} alt="Logo do code connect"/>
             <nav>
-                <ul>
+                <ul className="lista-sidebar">
                     <li>
-                        <a href="#">Publicar</a>
+                        <a href="#" className="item__item-publicacao">Publicar</a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="#" className="item__link item__link--ativo">
                             <img src={Feed} alt="" />
                             <span>Feed</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="#" className="item__link">
                             <img src={Perfil} alt="" />
                             <span>Perfil</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="#" className="item__link">
                             <img src={Info} alt="" />
                             <span>Sobre nós</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="#" className="item__link">
                             <img src={Sair} alt="" />
                             <span>Sair</span>
                         </a>
